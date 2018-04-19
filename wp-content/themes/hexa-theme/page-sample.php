bf5156e9aa82191d0ad3189109f20e268d751e3e
VuHoangNguyen <vuhoangnguyen49@gmail.com>
2018-04-19 10:14:10 +0700

    add page archive

--- wp-content/themes/hexa-theme/page-sample.php
+++ wp-content/themes/hexa-theme/page-sample.php
@@ -1,9 +1,20 @@
 <?php
 /**
-* Template Name: sample
+* Template Name: Sample
 **/
 get_header();
 ?>
+  <!--======= CONTENT =========-->
+  <div class="inside-header">
+   <div class="container"><?php the_title(); ?>
+   </div>
+   </div>
+  <div class="content">
+    <div class="container">
 
-    
-<?php get_footer(); ?>
+
+  	</div>
+  </div>
+
+  ?>
+  <?php get_footer(); ?>
